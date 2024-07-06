3c99d41d2ba51f784c886cdca211c82bc122397b
shuhasegawa <shuhasegawa0430@gmail.com>
2024-07-06 11:48:55 +0900

    use context

--- worldwise/src/contexts/CitiesContext.jsx
+++ worldwise/src/contexts/CitiesContext.jsx
@@ -1,40 +1,116 @@
-import { createContext, useContext, useEffect, useState } from 'react';
+import { createContext, useContext, useEffect, useReducer } from 'react';
 
 const BASE_URL = 'http://localhost:8000';
 
 const CitiesContext = createContext();
 
 function CitiesProvider({ children }) {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [currentCity, setCurrentCity] = useState({});
+  const initialState = {
+    cities: [],
+    isLoading: false,
+    currentCity: {},
+    error: '',
+  };
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case 'loading':
+        return { ...state, isLoading: true };
+      case 'cities/loaded':
+        return {
+          ...state,
+          isLoading: false,
+          cities: action.payload,
+        };
+      case 'city/loaded':
+        return {
+          ...state,
+          isLoading: false,
+          currentCity: action.payload,
+        };
+      case 'city/created':
+        return {
+          ...state,
+          isLoading: false,
+          cities: [...state.cities, action.payload],
+          currentCity: action.payload,
+        };
+      case 'city/deleted':
+        return {
+          ...state,
+          isLoading: false,
+          cities: state.cities.filter((city) => city.id !== action.payload),
+          currentCity: {},
+        };
+      case 'rejected':
+        return { ...state, isLoading: false, error: action.payload };
+      default:
+        throw new Error('Action Type unknown');
+    }
+  }
+
+  const [{ cities, isLoading, currentCity, error }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  // const [cities, setCities] = useState([]);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [currentCity, setCurrentCity] = useState({});
 
   useEffect(() => {
     async function fetchCities() {
       try {
-        setIsLoading(true);
+        dispatch({ type: 'loading' });
         const res = await fetch(`${BASE_URL}/cities`);
         const data = await res.json();
-        setCities(data);
+        dispatch({ type: 'cities/loaded', payload: data });
       } catch {
-        alert('fetching error');
-      } finally {
-        setIsLoading(false);
+        dispatch({ type: 'rejected', payload: 'fetching error cities' });
       }
     }
     fetchCities();
   }, []);
 
-  async function getCity(id) {
+  async function createCity(newCity) {
     try {
-      setIsLoading(true);
+      dispatch({ type: 'loading' });
+      const res = await fetch(`${BASE_URL}/cities/`, {
+        method: 'POST',
+        body: JSON.stringify(newCity),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const data = await res.json();
+      dispatch({ type: 'city/created', payload: data });
+    } catch {
+      dispatch({ type: 'rejected', payload: 'create city error' });
+    }
+  }
+
+  async function deleteCity(id) {
+    try {
+      dispatch({ type: 'loading' });
+      await fetch(`${BASE_URL}/cities/${id}`, {
+        method: 'DELETE',
+      });
+      dispatch({ type: 'city/deleted', payload: id });
+    } catch {
+      dispatch({ type: 'rejected', payload: 'delete city error' });
+    }
+  }
+
+  async function getCity(id) {
+    console.log(id, currentCity.id);
+    if (Number(id) === currentCity.id) return;
+    try {
+      dispatch({ type: 'loading' });
       const res = await fetch(`${BASE_URL}/cities/${id}`);
       const data = await res.json();
-      setCurrentCity(data);
+      dispatch({ type: 'city/loaded', payload: data });
     } catch {
-      alert('fetching error');
-    } finally {
-      setIsLoading(false);
+      dispatch({ type: 'rejected', payload: 'fetch city error' });
     }
   }
 
@@ -44,7 +120,10 @@ function CitiesProvider({ children }) {
         cities,
         isLoading,
         currentCity,
+        error,
         getCity,
+        createCity,
+        deleteCity,
       }}
     >
       {children}
--- worldwise/src/pages/AppLayout.jsx
+++ worldwise/src/pages/AppLayout.jsx
@@ -1,13 +1,19 @@
 import Map from '../components/Map';
 import Sidebar from '../components/Sidebar';
+import User from '../components/User';
+import { useAuth } from '../contexts/FakeAuthContext';
 import styles from './AppLayout.module.css';
 
 function AppLayout() {
+  const { isAuthenticated } = useAuth();
   return (
-    <div className={styles.app}>
-      <Sidebar />
-      <Map />
-    </div>
+    <>
+      <div className={styles.app}>
+        {isAuthenticated && <User />}
+        <Sidebar />
+        <Map />
+      </div>
+    </>
   );
 }
 
