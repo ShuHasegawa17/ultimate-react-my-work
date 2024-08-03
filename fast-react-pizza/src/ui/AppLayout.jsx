2fb08455cdf965db12bd2b1307059db231f95178
shuhasegawa <shuhasegawa0430@gmail.com>
2024-08-03 15:13:01 +0900

    tailwind practice

--- fast-react-pizza/src/ui/AppLayout.jsx
+++ fast-react-pizza/src/ui/AppLayout.jsx
@@ -9,15 +9,15 @@ function AppLayout() {
 
   console.log(navigation);
   return (
-    <div className="layout">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
-
       <Header />
-
-      <main>
-        {/* App.jsxで定義したchilderenが入る */}
-        <Outlet />
-      </main>
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl">
+          {/* App.jsxで定義したchilderenが入る */}
+          <Outlet />
+        </main>
+      </div>
       <CartOverview />
     </div>
   );
